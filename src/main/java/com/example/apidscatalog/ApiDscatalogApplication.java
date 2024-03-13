efaddaf82608afad71c364604133e9e10a639a9e
Alex Miguel <alexm@esparta.io>
2024-03-13 02:11:47 -0300

    initial commit

---
+++ src/main/java/com/example/apidscatalog/ApiDscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.apidscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDscatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apidscatalog/ApiDscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
